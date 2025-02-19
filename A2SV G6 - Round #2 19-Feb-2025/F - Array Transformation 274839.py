bfc24587140fee761c695397aab577dde7f4dd7a
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-02-19 19:25:19 +0300

    Created 'F - Array Transformation' by A2SV Hub

---
+++ A2SV G6 - Round #2 19-Feb-2025/F - Array Transformation 274839.py
@@ -0,0 +1,29 @@
+# Problem: F - Array Transformation - https://codeforces.com/gym/586960/problem/F
+
+from collections import Counter
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    
+    hT = Counter(arr)
+    freq = list(hT.values())
+
+    freqOffreq = Counter(freq)
+    
+
+    ans = float('inf')
+
+    for key in freqOffreq:
+        # Calculate the cost of making all frequencies equal to `key`
+        curr_cost = 0
+        for f in freqOffreq:
+            if f >= key:
+                curr_cost += ((f - key) * freqOffreq[f])
+            else:
+                curr_cost += ((f) * freqOffreq[f])
+
+        ans = min(ans, curr_cost)
+
+    print(ans)
