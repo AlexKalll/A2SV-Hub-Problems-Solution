dbfb6ce843667cc97511c40b98340975bf384025
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-02-20 10:55:55 +0300

    Created 'Linked List Cycle II' by A2SV Hub

---
+++ 20-Feb-2025/Linked List Cycle II 276035.py
@@ -0,0 +1,32 @@
+# Problem: Linked List Cycle II - https://leetcode.com/problems/linked-list-cycle-ii/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        tort = hare = head
+
+        is_cycled = False
+        while hare and hare.next:
+            tort = tort.next
+            hare = hare.next.next
+
+            if hare == tort:
+                is_cycled = True
+                break
+
+        if is_cycled:
+            tort = head 
+            
+            while tort != hare:
+                hare = hare.next
+                tort = tort.next
+            
+            return hare
+
+    
+        return 
