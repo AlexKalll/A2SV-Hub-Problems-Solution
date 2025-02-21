19d1fffe832c02b206374e5426897a723afb560e
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-02-21 09:45:44 +0300

    Created 'Find the Duplicate Number' by A2SV Hub

---
+++ 21-Feb-2025/Find the Duplicate Number 277933.py
@@ -0,0 +1,19 @@
+# Problem: Find the Duplicate Number - https://leetcode.com/problems/find-the-duplicate-number/
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        # floyd's tort and hare approach for cycle detection and finding entrance node ( fast & slow pointers approach)
+        hare = tort = 0
+
+        while True:
+            hare = nums[nums[hare]]
+            tort = nums[tort]
+
+            if tort == hare:
+                tort = 0
+
+                while tort != hare:
+                    tort = nums[tort]
+                    hare = nums[hare]
+
+                return tort
