9a82bf72b763ffe3b6033b152c12f0b0c9bb2643
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-02-26 19:16:56 +0300

    Created 'B - Jo's Adventure' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Feb-2025/B - Jo's Adventure 285121.py
@@ -0,0 +1,35 @@
+# Problem: B - Jo's Adventure - https://codeforces.com/gym/590053/problem/B
+
+n, m = map(int, input().split())
+height = list(map(int, input().split()))
+
+query = []
+for _ in range(m):
+    query.append(list(map(int, input().split())))
+
+pf = [0] * (n)
+for i in range(1, n):
+    if height[i] < height[i - 1]:
+        pf[i] += height[i - 1] - height[i] 
+    pf[i] += pf[i - 1]
+
+# print(pf)
+
+pf_back = [0] * (n)
+height.reverse()
+
+for i in range(1, len(height)):
+    if height[i] < height[i - 1]:
+        pf_back[i] += height[i - 1] - height[i] 
+    pf_back[i] += pf_back[i - 1]
+
+# print(pf)
+# print(pf_back)
+
+for l, r in query:
+    if r < l:
+        print(pf_back[n - r] - pf_back[n - l])
+    else:
+        print(pf[r-1] - pf[l-1])
+
+
