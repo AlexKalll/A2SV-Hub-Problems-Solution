40d3d98a792846403cd590ad656cd539ec45c5e9
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-02-26 19:55:07 +0300

    Created 'I - The Odd Challenge' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 26-Feb-2025/I - The Odd Challenge 285218.py
@@ -0,0 +1,24 @@
+# Problem: I - The Odd Challenge - https://codeforces.com/gym/589822/problem/I
+
+t = int(input())
+for _ in range(t):
+    n, q = map(int, input().split())
+    a = list(map(int, input().split()))
+
+    for i in range(1, len(a)):
+        a[i] += a[i-1]
+    # print(a)
+    for _ in range(q):
+        l, r, k = map(int, input().split())
+        curr = (r - l + 1) * k 
+        if l >= 2:
+            total = a[-1] + curr - (a[r-1] - a[l-2])
+        else:
+            total = a[-1] + curr - (a[r-1])
+
+        if total % 2 != 0:
+            print("YES")
+        else:
+            print("NO")
+            
+
