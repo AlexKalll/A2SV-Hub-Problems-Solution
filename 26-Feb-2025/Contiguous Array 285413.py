aa69ea62c821af7b59c29f2182a3b54f0280f940
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-02-26 22:13:50 +0300

    Created 'Contiguous Array' by A2SV Hub

---
+++ 26-Feb-2025/Contiguous Array 285413.py
@@ -0,0 +1,17 @@
+# Problem: Contiguous Array - https://leetcode.com/problems/contiguous-array/
+
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        
+        curr = 0
+        ht = {0: -1}
+        ans = 0
+
+        for i, num in enumerate(nums):
+            curr += 1 if num == 1 else -1
+            if curr in ht:
+                ans = max(ans, i - ht[curr])
+            else:
+                ht[curr] = i
+        
+        return ans 
