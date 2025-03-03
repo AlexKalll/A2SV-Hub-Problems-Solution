b4188d3cb1ef8e043871be535267780419a60d36
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-03 20:40:37 +0300

    Created 'Partition Array According to Given Pivot' by A2SV Hub

---
+++ 03-Mar-2025/Partition Array According to Given Pivot 289963.py
@@ -0,0 +1,17 @@
+# Problem: Partition Array According to Given Pivot - https://leetcode.com/problems/partition-array-according-to-given-pivot/description/
+
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        less = []
+        greater = []
+        cnt = 0
+
+        for num in nums:
+            if num < pivot:
+                less.append(num)
+            elif num > pivot:
+                greater.append(num)
+            else:
+                cnt += 1
+
+        return less + [pivot] * cnt + greater
