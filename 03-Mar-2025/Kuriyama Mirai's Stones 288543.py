a2d3e26d1c58a9fcc964adf5b2c36bb25a2dc667
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-03 07:54:53 +0300

    Created 'Kuriyama Mirai's Stones' by A2SV Hub

---
+++ 03-Mar-2025/Kuriyama Mirai's Stones 288543.py
@@ -0,0 +1,30 @@
+# Problem: Kuriyama Mirai's Stones - https://codeforces.com/contest/433/problem/B
+
+n = int(input())
+costs = list(map(int, input().split()))
+
+m = int(input())
+queries = []
+for _ in range(m):
+    type, l , r = map(int, input().split())
+    queries.append((type, l, r))
+
+prefix = [costs[0]]
+for i in range(1, n):
+    prefix.append(prefix[-1] + costs[i])
+
+costs.sort()
+sorted_prefix = [costs[0]]
+for i in range(1, n):
+    sorted_prefix.append(sorted_prefix[-1] + costs[i])
+
+# print(prefix)
+# print(sorted_prefix)
+
+for type, l, r in queries:
+    if type == 1:
+        ans = prefix[r-1] - (prefix[l-2] if l-2 >= 0 else 0)
+    else:
+        ans = sorted_prefix[r-1] - (sorted_prefix[l-2] if l-2 >= 0 else 0)
+    
+    print(ans)
