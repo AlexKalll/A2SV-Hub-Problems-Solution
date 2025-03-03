02b881423cb0f5d98fd52558acc4c3a84712a7b9
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-03 22:10:06 +0300

    Created 'D - Life Across the Stars' by A2SV Hub

---
+++ A2SV G6 - Round #5 03-Mar-2025/D - Life Across the Stars 290060.py
@@ -0,0 +1,29 @@
+# Problem: D - Life Across the Stars - https://codeforces.com/gym/591928/problem/D
+
+from collections import defaultdict
+
+n = int(input())
+
+ht = defaultdict(int)
+for _ in range(n):
+    b, d = map(int, input().split())
+    ht[b] += 1
+    ht[d] -= 1
+# print(years)
+
+ht = dict(sorted(ht.items()))
+
+# print(ht)
+
+ans = 0
+idx = 0
+pf= 0
+
+for key in ht:
+    pf += ht[key]
+
+    if pf > ans:
+        ans = pf
+        idx = key
+
+print(idx, ans)
