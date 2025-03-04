3a83222027cad60abbe95fa1650e325c3571eb80
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-04 19:08:47 +0300

    Created 'Rabbits in Forest' by A2SV Hub

---
+++ 04-Mar-2025/Rabbits in Forest 291833.py
@@ -0,0 +1,14 @@
+# Problem: Rabbits in Forest - https://leetcode.com/problems/rabbits-in-forest/
+
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        ht = Counter(answers)
+        count = len(answers)
+        
+        for key in ht:
+            same_color = key + 1 
+            rem = ht[key] % (same_color)
+            if rem:
+                count += same_color - rem
+
+        return count
