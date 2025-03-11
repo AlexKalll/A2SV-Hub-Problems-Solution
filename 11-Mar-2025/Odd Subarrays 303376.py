7c9d8c31a13237a3010de367a9be83ea06673ee3
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-11 23:27:17 +0300

    Created 'Odd Subarrays' by A2SV Hub

---
+++ 11-Mar-2025/Odd Subarrays 303376.py
@@ -0,0 +1,30 @@
+# Problem: Odd Subarrays - https://codeforces.com/problemset/problem/1686/B
+
+import sys, math
+from collections import Counter, defaultdict, deque  
+from inspect import currentframe as  cf
+
+input = sys.stdin.readline  
+def iinput(): return (int(input()))  
+def linput(): return (list(map(int, input().split())))  
+def sinput(): return (input().strip())  
+def minput(): return (map(int, input().split()))  
+
+t = int(input())
+for _ in range(t):
+
+	n = iinput()
+	arr = linput()
+
+	cnt = 0
+	l = 1
+	while l < n:
+		if arr[l] < arr[l - 1]:
+			cnt +=1 
+			l += 1
+		l += 1
+
+	print(cnt)	
+
+
+
