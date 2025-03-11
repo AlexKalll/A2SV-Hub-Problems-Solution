3d4499dbbdd4b37c9c45ac6356e6a78dc348330b
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-11 17:58:41 +0300

    Created 'Factorial Trailing Zeroes' by A2SV Hub

---
+++ 11-Mar-2025/Factorial Trailing Zeroes 302676.py
@@ -0,0 +1,22 @@
+# Problem: Factorial Trailing Zeroes - https://leetcode.com/problems/factorial-trailing-zeroes/
+
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        
+        def factorial(n):
+            if n <= 1:
+                return 1
+            
+            return n * factorial(n-1)
+
+        fact = factorial(n)
+
+        ans = 0
+        while fact % 10 == 0:
+            fact //= 10
+            ans += 1
+        
+        return ans 
+        
+        
+        
