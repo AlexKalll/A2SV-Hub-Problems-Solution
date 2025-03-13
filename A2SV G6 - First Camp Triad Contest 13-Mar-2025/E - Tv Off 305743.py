42adeccfe78e695e668c68571f1d9145a7194d2c
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-13 21:59:16 +0300

    Created 'E - Tv Off' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 13-Mar-2025/E - Tv Off 305743.py
@@ -0,0 +1,36 @@
+# Problem: E - Tv Off - https://codeforces.com/gym/589822/problem/E
+
+n = int(input())
+segments = []
+segset = set()
+
+for _ in range(n):
+    l, r = map(int, input().split())
+    segments.append((l, r+1))
+    segset.add(l)
+    segset.add(r+1)
+    
+seglist = sorted(segset)
+# print(listofseg)
+segmap = {v:i for i, v in enumerate(seglist)}
+# print(segmap)
+m = len(seglist)
+
+coverage = [0] * (m+1)
+for l, r in segments:
+    coverage[segmap[l]] += 1
+    coverage[segmap[r]] -= 1
+    
+for i in range(1, m):
+    coverage[i] += coverage[i-1]
+
+pref = [0] * m
+for i in range(1, m):
+    pref[i] = pref[i-1] + (1 if coverage[i-1] == 1 else 0)
+    
+for i, (l, r) in enumerate(segments):
+    if pref[segmap[r]] - pref[segmap[l]] == 0:
+        print(i+1)
+        break
+else:
+    print(-1)
