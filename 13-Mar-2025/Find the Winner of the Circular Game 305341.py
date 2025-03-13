84e591d854b40fe49f3b3a39a022a4694432ce0d
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-13 19:20:32 +0300

    Created 'Find the Winner of the Circular Game' by A2SV Hub

---
+++ 13-Mar-2025/Find the Winner of the Circular Game 305341.py
@@ -0,0 +1,18 @@
+# Problem: Find the Winner of the Circular Game - https://leetcode.com/problems/find-the-winner-of-the-circular-game/
+
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        player = [a for a in range(1, n+1)]
+
+        def helper(player, cur):
+            
+
+            if len(player) == 1:
+                return player[0]
+
+            cur = (cur + k -1) % len(player)
+            player.pop(cur)
+            
+            return helper(player, cur)
+            
+        return helper(player, 0)
