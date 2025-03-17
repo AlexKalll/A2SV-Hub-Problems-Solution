1c613ec73e8c6a00716ac1e9630116c9d8c7d7d3
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-17 18:38:09 +0300

    Created 'Search in a Binary Search Tree' by A2SV Hub

---
+++ 17-Mar-2025/Search in a Binary Search Tree 309373.py
@@ -0,0 +1,43 @@
+# Problem: Search in a Binary Search Tree - https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+"""
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+
+        def dfs(node, val):
+            if not node:
+                return None
+                
+            if val == node.val:
+                return node
+
+            elif val < node.val:
+                return dfs(node.left, val)
+            else:
+                return dfs(node.right, val)
+        
+        return helper(root, val)
+            
+"""
+# BFS approach 
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+
+        q = deque([root])
+        while q:
+            node = q.popleft()
+            if node.val == val:
+                return node
+            
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+            
+        return None
