a4605802d381cbbfedc2ceb0519636dc6b16f18d
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-17 18:48:24 +0300

    Created 'Duplicate Zeros' by A2SV Hub

---
+++ 17-Mar-2025/Duplicate Zeros 309413.py
@@ -0,0 +1,15 @@
+# Problem: Duplicate Zeros - https://leetcode.com/problems/duplicate-zeros/description/?envType=problem-list-v2&envId=two-pointers
+
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        i = 0
+        while i < len(arr):
+            if arr[i] == 0:
+                arr.insert(i, 0)
+                arr.pop()
+                i += 1
+                
+            i += 1
