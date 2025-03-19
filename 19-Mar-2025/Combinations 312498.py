5fa934a43dac24d9eeaaa43f530532190de1f3e6
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-19 18:19:06 +0300

    Created 'Combinations' by A2SV Hub

---
+++ 19-Mar-2025/Combinations 312498.py
@@ -0,0 +1,13 @@
+# Problem: Combinations - https://leetcode.com/problems/combinations/
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        arr = [num for num in range(1, n+1)]
+        
+        combs = combinations(arr, k)
+        ans = []
+        
+        for comb in combs:
+            ans.append(list(comb))
+        
+        return ans
