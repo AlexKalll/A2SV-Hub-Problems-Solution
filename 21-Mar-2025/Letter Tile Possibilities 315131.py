2a85efbb613cd9e6c16981cb78e26216651d454f
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-21 21:18:53 +0300

    Created 'Letter Tile Possibilities' by A2SV Hub

---
+++ 21-Mar-2025/Letter Tile Possibilities 315131.py
@@ -0,0 +1,12 @@
+# Problem: Letter Tile Possibilities - https://leetcode.com/problems/letter-tile-possibilities/description/
+
+class Solution(object):
+    def numTilePossibilities(self, tiles):
+        s=set()
+        tiles=list(tiles)
+        for i in range(1,len(tiles)+1):
+            x=permutations(tiles,i)
+            for i in x:
+                s.add(i)
+        return len(s)
+        
