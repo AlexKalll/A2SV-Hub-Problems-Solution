dabf1b845848d5a44bfc8843412a71b0261b7ff0
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-24 20:53:21 +0300

    Created 'Generate Binary Strings Without Adjacent Zeros' by A2SV Hub

---
+++ 24-Mar-2025/Generate Binary Strings Without Adjacent Zeros 317145.py
@@ -0,0 +1,20 @@
+# Problem: Generate Binary Strings Without Adjacent Zeros - https://leetcode.com/problems/generate-binary-strings-without-adjacent-zeros/description/
+
+class Solution:
+    def validStrings(self, n: int) -> List[str]:
+        ans = []
+
+        def backtrack(curr):
+            if len(curr) == n:
+                ans.append(''.join(curr[:]))
+                return 
+
+            for bit in '01':
+                if curr and curr[-1] == '0' and bit == '0':
+                    continue
+                curr.append(bit)
+                backtrack(curr)
+                curr.pop()
+        
+        backtrack([])
+        return ans
