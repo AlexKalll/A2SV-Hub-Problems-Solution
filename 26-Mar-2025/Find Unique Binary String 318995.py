5a1e27b2f3041c79cf48e3fab7b54a527096fe9b
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-26 18:45:10 +0300

    Created 'Find Unique Binary String' by A2SV Hub

---
+++ 26-Mar-2025/Find Unique Binary String 318995.py
@@ -0,0 +1,22 @@
+# Problem: Find Unique Binary String - https://leetcode.com/problems/find-unique-binary-string/description/
+
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        nums = set(nums)
+        
+        def backtrack(curr):
+            if len(curr) == len(nums):
+                if ''.join(curr) not in nums:
+                    return True
+
+                return False
+
+            for bit in '01':
+                curr.append(bit)
+                if backtrack(curr):
+                    return ''.join(curr)
+                curr.pop()
+
+            return ''
+        
+        return backtrack([])
