ad4c5aee20f5d1e61d6958d5884cfdf005aaab94
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-30 23:30:59 +0300

    Created 'Number of Subsequences That Satisfy the Given Sum Condition' by A2SV Hub

---
+++ 30-Mar-2025/Number of Subsequences That Satisfy the Given Sum Condition 322248.py
@@ -0,0 +1,22 @@
+# Problem: Number of Subsequences That Satisfy the Given Sum Condition - https://leetcode.com/problems/number-of-subsequences-that-satisfy-the-given-sum-condition/
+
+class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        mod = 10**9 + 7
+        left, right = 0, len(nums) - 1
+        result = 0
+        
+        power = [1] * (len(nums) + 1)
+        
+        for i in range(1, len(nums) + 1):
+            power[i] = (power[i - 1] * 2) % mod
+        
+        while left <= right:
+            if nums[left] + nums[right] <= target:
+                result = (result + power[right - left]) % mod
+                left += 1
+            else:
+                right -= 1
+        
+        return result
