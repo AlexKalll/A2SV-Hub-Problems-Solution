9e178454fb4eb86ce9822c204d4ad283552ad420
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-03-31 22:09:36 +0300

    Created 'Petya and Strings' by A2SV Hub

---
+++ 31-Mar-2025/Petya and Strings 322823.py
@@ -0,0 +1,16 @@
+# Problem: Petya and Strings - https://codeforces.com/problemset/problem/112/A
+
+li = list(input())
+li1 = list(input())
+res = 0
+for i in range(len(li)):
+    if ord(li[i].lower()) < ord(li1[i].lower()):
+        res = -1
+        break
+    elif ord(li[i].lower()) > ord(li1[i].lower()):
+        res = 1
+        break
+if res == 0:
+    print(0)
+else:
+    print(res)
