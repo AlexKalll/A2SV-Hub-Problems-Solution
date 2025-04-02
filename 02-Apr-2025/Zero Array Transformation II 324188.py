3e36ca357e69f917a629e2c85f4e8df50b3e8819
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-04-02 19:53:41 +0300

    Created 'Zero Array Transformation II' by A2SV Hub

---
+++ 02-Apr-2025/Zero Array Transformation II 324188.py
@@ -0,0 +1,27 @@
+# Problem: Zero Array Transformation II - https://leetcode.com/problems/zero-array-transformation-ii/
+
+class Solution:
+    def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+        n = len(nums)
+        sum_val = 0
+        k = 0
+        diff_arr = [0] * (n + 1)
+
+        for i in range(n):
+            while sum_val + diff_arr[i] < nums[i]:
+                k += 1
+                if k > len(queries):
+                    return -1
+
+                left, right, val = queries[k-1]
+
+                if right >= i:
+                    diff_arr[max(left, i)] += val
+
+                    if right + 1 < len(diff_arr):
+                        diff_arr[right + 1] -= val
+
+            sum_val += diff_arr[i]
+
+        # print(diff_arr)
+        return k
