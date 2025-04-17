fe414b9dfc700e1b2a3b9ff0f97bcb9f292d128b
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-04-17 23:39:02 +0300

    Created 'Set Mismatch' by A2SV Hub

---
+++ 17-Apr-2025/Set Mismatch 336662.py
@@ -0,0 +1,21 @@
+# Problem: Set Mismatch - https://leetcode.com/problems/set-mismatch/description/
+
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        num_count = {}
+        duplicate = missing = -1
+
+        for num in nums:
+            if num in num_count:
+                num_count[num] += 1
+            else:
+                num_count[num] = 1
+    
+        for i in range(1, len(nums) + 1):
+            if i in num_count:
+                if num_count[i] == 2:
+                    duplicate = i
+            else:
+                missing = i
+        
+        return [duplicate, missing]
