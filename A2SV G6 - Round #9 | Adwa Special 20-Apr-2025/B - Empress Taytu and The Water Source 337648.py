313cb66cc40863785ce175fa244660a48a63a016
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-04-20 10:58:32 +0300

    Created 'B - Empress Taytu and The Water Source' by A2SV Hub

---
+++ A2SV G6 - Round #9 | Adwa Special 20-Apr-2025/B - Empress Taytu and The Water Source 337648.py
@@ -0,0 +1,28 @@
+# Problem: B - Empress Taytu and The Water Source - https://codeforces.com/gym/601269/problem/B
+
+def check(demands, time, cand, k):
+    total = 0
+    for i in range(len(demands)):
+        trips = (demands[i] + cand - 1) // cand
+        total += trips * time[i]
+        if total > k:
+            return False
+    return total <= k
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    d = list(map(int, input().split()))
+    a = list(map(int, input().split()))
+    
+    left, right = 1, max(d)
+    ans = -1
+    while left <= right:
+        mid = (left + right) // 2
+        if check(d, a, mid, k):
+            ans = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    print(ans)
