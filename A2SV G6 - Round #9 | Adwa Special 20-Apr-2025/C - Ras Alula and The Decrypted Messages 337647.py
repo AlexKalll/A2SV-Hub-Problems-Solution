22bf1ec32d19d748074380414fdf349c5bf0689c
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-04-20 10:58:27 +0300

    Created 'C - Ras Alula and The Decrypted Messages' by A2SV Hub

---
+++ A2SV G6 - Round #9 | Adwa Special 20-Apr-2025/C - Ras Alula and The Decrypted Messages 337647.py
@@ -0,0 +1,27 @@
+# Problem: C - Ras Alula and The Decrypted Messages - https://codeforces.com/gym/601269/problem/C
+
+
+def can_(s, w):
+    w_count = sum(ord(c) for c in w)
+    s_count = sum(ord(c) for c in s[:m])
+    
+    for i in range(m, n):
+        if s_count == w_count:
+            print("YES")
+            return
+        s_count += ord(s[i]) 
+        s_count -= ord(s[i-m])
+        
+    if s_count == w_count:
+        print("YES")
+        return
+
+    print("NO")
+
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split())
+    s = input()
+    w = input()
+
+    can_(s, w)
