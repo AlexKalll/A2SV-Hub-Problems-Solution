af05a3cda196241e9b88a8d971b6af018cbb89cc
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-04-20 10:58:45 +0300

    Created 'A - Escaping Prison' by A2SV Hub

---
+++ A2SV G6 - Round #9 | Adwa Special 20-Apr-2025/A - Escaping Prison 337649.py
@@ -0,0 +1,15 @@
+# Problem: A - Escaping Prison - https://codeforces.com/gym/601269/problem/A
+
+t = int(input())
+for _ in range(t):
+    n, h = map(int, input().split())
+    blanket = []
+    total_length = 0
+    for _ in range(n):
+        w, l = map(int, input().split())
+        total_length += max(w, l)
+
+    if total_length >= h:
+        print("YES")
+    else:
+        print("NO")
