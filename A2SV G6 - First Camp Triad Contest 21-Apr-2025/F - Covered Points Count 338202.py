f9b013ae137b65974db78e715d4819724e6134ab
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-04-21 20:55:52 +0300

    Created 'F - Covered Points Count' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 21-Apr-2025/F - Covered Points Count 338202.py
@@ -0,0 +1,33 @@
+# Problem: F - Covered Points Count - https://codeforces.com/gym/589822/problem/F
+
+n = int(input())
+events = []
+
+# Read the segments and create events for the sweep line
+for _ in range(n):
+    l, r = map(int, input().split())
+    events.append((l, 1))  
+    events.append((r + 1, -1))  
+
+# Sort events: first by coordinate, then by type (end before start if same coordinate)
+events.sort()
+
+# To count the number of points covered by exactly k segments
+cnt = [0] * (n + 1)
+
+current_coverage = 0
+last_position = -1
+
+# Sweep through the events
+for position, type in events:
+    if last_position != -1 and current_coverage > 0:
+        # Count the number of integer points between last_position and current position
+        cnt[current_coverage] += position - last_position
+
+    # Update the current coverage
+    current_coverage += type
+    last_position = position
+
+# print(cnt)
+
+print(*cnt[1:])
