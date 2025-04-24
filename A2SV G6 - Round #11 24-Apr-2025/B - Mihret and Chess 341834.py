68a992ec15c435aea7befba057043ce19594a4f2
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-04-24 22:28:55 +0300

    Created 'B - Mihret and Chess' by A2SV Hub

---
+++ A2SV G6 - Round #11 24-Apr-2025/B - Mihret and Chess 341834.py
@@ -0,0 +1,19 @@
+# Problem: B - Mihret and Chess - https://codeforces.com/gym/604781/problem/B
+
+r1, c1, r2, c2 = map(int, input().split())
+
+if r1 == r2 or c1 == c2:
+    rook_moves = 1
+else:
+    rook_moves = 2
+
+if (r1 + c1) % 2 != (r2 + c2) % 2:
+    bishop_moves = 0
+elif abs(r1 - r2) == abs(c1 - c2):
+    bishop_moves = 1
+else:
+    bishop_moves = 2
+
+king_moves = max(abs(r1 - r2), abs(c1 - c2))
+
+print(rook_moves, bishop_moves, king_moves)
