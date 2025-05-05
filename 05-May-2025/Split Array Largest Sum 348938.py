aa556454dbb51f360fef9f4a2edda41d032af032
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-05-05 23:05:46 +0300

    Created 'Split Array Largest Sum' by A2SV Hub

---
+++ 05-May-2025/Split Array Largest Sum 348938.py
@@ -0,0 +1,26 @@
+# Problem: Split Array Largest Sum - https://leetcode.com/problems/split-array-largest-sum/
+
+class Solution(object):
+    def splitArray(self, nums, k):
+
+        def validator(target):
+            count = 1
+            prefix = 0
+            for i in range(len(nums)):
+                prefix += nums[i]
+                if prefix > target:
+                    prefix = nums[i]
+                    count += 1
+            return count <= k
+
+        low,high = max(nums),sum(nums)
+        while low <= high:
+            mid = (low+high)//2
+            if validator(mid):
+                high = mid-1
+            else:
+                low = mid+1
+        return low
+        
+
+        
