ecd05ea5c25d6b0debe11e1e7baf6f603dfb9b58
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-05-07 17:38:12 +0300

    Created 'Number of Pairs Satisfying Inequality' by A2SV Hub

---
+++ 07-May-2025/Number of Pairs Satisfying Inequality 350122.py
@@ -0,0 +1,15 @@
+# Problem: Number of Pairs Satisfying Inequality - https://leetcode.com/problems/number-of-pairs-satisfying-inequality/
+
+from sortedcontainers import SortedList
+
+class Solution:
+    def numberOfPairs(self, nums1: List[int], nums2: List[int], diff: int) -> int:
+        deltas = SortedList()
+        pairs = 0 
+        for num1, num2 in zip(nums1, nums2):
+            delta = num1 - num2
+            
+            pairs += deltas.bisect_right(delta + diff)
+            deltas.add(delta)
+            
+        return pairs
