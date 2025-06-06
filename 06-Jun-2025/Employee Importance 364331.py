2489103a7942530de22b8dfae1b17cd9ee105bab
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-06-06 21:28:07 +0300

    Created 'Employee Importance' by A2SV Hub

---
+++ 06-Jun-2025/Employee Importance 364331.py
@@ -0,0 +1,23 @@
+# Problem: Employee Importance - https://leetcode.com/problems/employee-importance/
+
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        d = {j.id:i for i, j in enumerate(employees)}
+        s = [id] #stack
+        ans = 0
+
+        while(s):
+            curr = s.pop()
+            ans += employees[d[curr]].importance
+            for child in employees[d[curr]].subordinates:             
+                s.append(child)
+        return ans
