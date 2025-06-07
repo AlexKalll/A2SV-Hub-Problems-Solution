e0f2c89edc63accb17bd57c7b91f259d1271d0fd
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-06-07 11:29:58 +0300

    Created 'Find Champion II' by A2SV Hub

---
+++ 07-Jun-2025/Find Champion II 364464.py
@@ -0,0 +1,17 @@
+# Problem: Find Champion II - https://leetcode.com/problems/find-champion-ii/
+
+class Solution:
+    def findChampion(self, n: int, edges: List[List[int]]) -> int:
+        count = defaultdict(int)
+
+        for u, v in edges:
+            count[v] += 1
+
+        ans, parentCount = 0, 0
+        for i in range(n):
+            if not count[i]:
+                ans = i
+                parentCount += 1
+            if parentCount == 2:
+                return -1
+        return ans
