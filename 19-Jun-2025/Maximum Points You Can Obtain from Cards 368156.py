839c5f0dbbf9b3d4a4a876876e692d6a0c4e240c
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-06-19 20:37:13 +0300

    Created 'Maximum Points You Can Obtain from Cards' by A2SV Hub

---
+++ 19-Jun-2025/Maximum Points You Can Obtain from Cards 368156.py
@@ -0,0 +1,16 @@
+# Problem: Maximum Points You Can Obtain from Cards - https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
+
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        n = len(cardPoints)
+        
+        # Calculate the total sum of the first k cards
+        total = sum(cardPoints[:k])
+        max_score = total
+        
+        # Use a sliding window to consider taking cards from the end
+        for i in range(1, k + 1):
+            total = total - cardPoints[k - i] + cardPoints[n - i]
+            max_score = max(max_score, total)
+        
+        return max_score
