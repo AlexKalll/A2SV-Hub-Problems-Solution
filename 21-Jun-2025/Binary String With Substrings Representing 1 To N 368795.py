dfac02d1f45d962419842aa19d000905ec4f19b8
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-06-21 05:37:10 +0300

    Created 'Binary String With Substrings Representing 1 To N' by A2SV Hub

---
+++ 21-Jun-2025/Binary String With Substrings Representing 1 To N 368795.py
@@ -0,0 +1,12 @@
+# Problem: Binary String With Substrings Representing 1 To N - https://leetcode.com/problems/binary-string-with-substrings-representing-1-to-n/
+
+class Solution:
+    def queryString(self, s: str, n: int) -> bool:
+        l=[]
+        for i in range(len(s)):
+            for j in range(i+1,len(s)+1):
+                l.append(s[i:j])
+        for i in range(1,n+1):
+            if bin(i)[2:] not in l:
+                return False
+        return True
