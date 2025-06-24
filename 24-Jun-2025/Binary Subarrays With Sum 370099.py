d66f66922a8185a0c845495813472c157719de5f
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-06-24 20:14:06 +0300

    Created 'Binary Subarrays With Sum' by A2SV Hub

---
+++ 24-Jun-2025/Binary Subarrays With Sum 370099.py
@@ -0,0 +1,15 @@
+# Problem: Binary Subarrays With Sum - https://leetcode.com/problems/binary-subarrays-with-sum/
+
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        pref_sum = 0
+        count = 0
+        sum_count = Counter({0 : 1})
+
+        for bit in nums:
+            pref_sum += bit
+            count += sum_count[pref_sum - goal]
+            
+            sum_count[pref_sum] += 1
+        
+        return count
