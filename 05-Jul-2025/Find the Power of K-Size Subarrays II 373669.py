af7c559646669215058d096d7feafcdf1a162b16
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-07-05 20:25:28 +0300

    Created 'Find the Power of K-Size Subarrays II' by A2SV Hub

---
+++ 05-Jul-2025/Find the Power of K-Size Subarrays II 373669.py
@@ -0,0 +1,16 @@
+# Problem: Find the Power of K-Size Subarrays II - https://leetcode.com/problems/find-the-power-of-k-size-subarrays-ii/
+
+class Solution:
+    def resultsArray(self, nums: List[int], k: int) -> List[int]:
+        if k == 1:#
+            return nums
+        ans, cnt = [], 1
+        for i, (x, y) in enumerate(pairwise(nums), 2):
+            if x + 1 == y: 
+                cnt += 1
+            else:
+                cnt = 1        
+            if i >= k:
+                ans.append(y if cnt >= k else -1)
+        return ans
+        
