d2f43b91ec80461235818eb0aa77452894598f40
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-07-07 22:17:43 +0300

    Created 'Find the Substring With Maximum Cost' by A2SV Hub

---
+++ 07-Jul-2025/Find the Substring With Maximum Cost 374360.py
@@ -0,0 +1,11 @@
+# Problem: Find the Substring With Maximum Cost - https://leetcode.com/problems/find-the-substring-with-maximum-cost/
+
+class Solution:
+    def maximumCostSubstring(self, s: str, chars: str, vals: List[int]) -> int:
+        m = dict(zip(chars, vals))
+        res = cur = 0
+        for c in s:
+            cur = max(cur + m.get(c, ord(c) - ord('a') + 1), 0)
+            res = max(res, cur)
+        return res
+        
