4c69b6ce26f8027cc627b03e5fe81bcb143a0329
Kaletsidik Ayalew <161981213+AlexKalll@users.noreply.github.com>
2025-07-29 21:40:46 +0300

    Created 'Count Number of Maximum Bitwise-OR Subsets' by A2SV Hub

---
+++ 29-Jul-2025/Count Number of Maximum Bitwise-OR Subsets 380333.py
@@ -0,0 +1,27 @@
+# Problem: Count Number of Maximum Bitwise-OR Subsets - https://leetcode.com/problems/count-number-of-maximum-bitwise-or-subsets/description/
+
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        subsets = []
+        def backtrack(start, path):
+            if path: # ths helps not to append the empty set 
+                subsets.append(path.copy())
+            for i in range(start, len(nums)):
+                backtrack(i+1, path + [nums[i]])
+
+        backtrack(0, [])
+
+        max_bitwise = 0
+        for num in nums:
+            max_bitwise |= num
+
+        count = 0
+        for subset in subsets:
+            curr = 0
+            for num in subset:
+                curr |= num
+            
+            if curr == max_bitwise:
+                count += 1
+
+        return count
